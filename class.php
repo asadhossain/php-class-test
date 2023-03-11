a8c15404a264c83e884350c143dc4a2425f9ab6b
asadhossain <developerasad007@gmail.com>
2023-03-12 02:20:19 +0600

    asad

--- class.php
+++ class.php
@@ -4,15 +4,14 @@ class MyClass{
     public $x, $y, $z;
 
     public function mysum(){
-        $this->z = $this->x +$this->y;
+        $this->z = $this->x + $this->y;
+        return $this->z;
 
     }
-
-
-
 }
 
 
+$asad = new MyClass();
 
 
 
