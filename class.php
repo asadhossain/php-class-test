db9acd26ff0c43c93c5935ffa48e10d5b85b6444
asadhossain <developerasad007@gmail.com>
2023-03-12 02:51:48 +0600

    asad

--- class.php
+++ class.php
@@ -3,6 +3,12 @@
 class MyClass{
     public $a, $x, $y, $z;
 
+    public function __construct($b, $c){
+        $this->x = $b;
+        $this->y = $c;
+
+    }
+
     public function mysum(){
         $this->z = $this->x + $this->y - $this->a;
         return $this->z;
